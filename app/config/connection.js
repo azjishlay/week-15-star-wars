8303f6ae34ef3c3bef365d19232aa41085a9fef0
lijasminej <li.jasmine.j@gmail.com>
2016-05-07 11:00:23 -0400

    Update jawsDB connection

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,11 +1,22 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'starwars'
-});
+
+var source = {
+  localhost: {
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'starwars'
+  },
+  jawsDB: {
+    port: 3306,
+    host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'w46dywm0js66cdaz',
+    password: 'elgz2ebe97nitkrc',
+    // database: 'starwars' // connect to remote database
+  }
+}
+var connection = mysql.createConnection(source.jawsDB);
 
 connection.connect(function(err){
   if (err) {
